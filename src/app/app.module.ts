4bbfe67b3013e2857574ff5a3ef317ff8d92495f
guilherme.pereira <guilherme.pereira@7comm.com.br>
2021-03-21 02:53:41 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,81 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProdutoComponent } from './produto/produto.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+// Angular Material
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatDividerModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatExpansionModule,
+  MatRadioModule,
+  MatDialogModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatStepperModule,
+  MatTreeModule,
+  MatTabsModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatListModule,
+  MatToolbarModule,
+  MatSlideToggleModule,
+  MatPaginatorIntl,
+} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatCardModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatDividerModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatExpansionModule,
+  MatRadioModule,
+  MatDialogModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatStepperModule,
+  MatTreeModule,
+  MatTabsModule,
+  MatChipsModule,
+  MatAutocompleteModule,
+  MatListModule,
+  MatToolbarModule,
+  MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
